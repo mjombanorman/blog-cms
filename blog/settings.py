aaaa62f5d593544c472e53a920c54b0b87b8d9f5
mjombanorman <mjomban@gmail.com>
2023-11-25 13:11:27 +0300

    Finalizing the post page and ensuring the template is dynamically updated.

--- blog/settings.py
+++ blog/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -104,10 +104,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'assets'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
 MEDIA_URL = '/media/'
-MEDIA_ROOT =BASE_DIR /'upload'
+MEDIA_ROOT = BASE_DIR /'upload'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
