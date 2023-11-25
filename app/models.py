576fc223d55586e0e0118585c09b7d44c937078c
mjombanorman <mjomban@gmail.com>
2023-11-25 11:31:45 +0300

    setting up the Posts model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    last_update = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True,max_length=200)
+    image = models.ImageField(upload_to='post-images/',null=True,blank=True)
+
+    def __str__(self):
+        return self.title
