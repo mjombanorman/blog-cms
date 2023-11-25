395a3ae8aacb9e5c582faaf49464f26ac0e6cb57
mjombanorman <mjomban@gmail.com>
2023-11-25 12:07:07 +0300

    Setting up the posts view and templates including the base template

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from .models import Post
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'app/all_posts.html'
+    
+class PostDetailView(DetailView):
+    model = Post
+    context_object_name = 'post'
+    template_name = 'app/post.html'
