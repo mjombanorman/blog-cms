e0b1e7d36c348438ffdb49ef7e4e66419f1be85d
mjombanorman <mjomban@gmail.com>
2023-11-25 18:42:53 +0300

    Setting up tags within the applications

--- app/models.py
+++ app/models.py
@@ -1,11 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True,max_length=100)
+    
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        return super(Tag,self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
+        
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     last_update = models.DateTimeField(auto_now=True)
     slug = models.SlugField(unique=True,max_length=200)
     image = models.ImageField(upload_to='post-images/',null=True,blank=True)
-
+    tags = models.ManyToManyField(Tag,blank=True,related_name="post")
+   
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        return super(Post,self).save(*args,**kwargs)
+    
     def __str__(self):
         return self.title
+    
+    
